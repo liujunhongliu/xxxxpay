38376ccf6b28f6f0e3b18ee0ba4c2c9a5cfd2042
liujh <942055739@qq.com>
2018-12-17 17:18:48 +0800

    子项目模块

---
+++ mypay/src/main/java/com/jk/MypayApplication.java
@@ -0,0 +1,14 @@
+package com.jk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MypayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MypayApplication.class, args);
+    }
+
+}
+
